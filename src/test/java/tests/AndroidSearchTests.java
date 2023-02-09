8b44fc675b4d641aa3eab91cc834d514df9fbbed
ostepovoyv <ov.stepovoy@gmail.com>
2023-02-10 02:30:44 +0600

    fix

--- src/test/java/tests/AndroidSearchTests.java
+++ src/test/java/tests/AndroidSearchTests.java
@@ -1,18 +1,10 @@
 package tests;
 
-import org.junit.jupiter.api.Disabled;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import pages.WikipediaPage;
-
-import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
-import static io.appium.java_client.AppiumBy.accessibilityId;
-import static io.qameta.allure.Allure.step;
-import static org.openqa.selenium.By.id;
-
 
 public class AndroidSearchTests extends TestBase {
 
