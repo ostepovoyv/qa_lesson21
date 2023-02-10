1739df556a0f45923bf3aa87855f07fb1d76d168
ostepovoyv <ov.stepovoy@gmail.com>
2023-02-10 13:23:25 +0600

    fix

--- src/test/java/tests/IosSearchTests.java
+++ src/test/java/tests/IosSearchTests.java
@@ -6,11 +6,6 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import pages.IosPage;
 
-import static com.codeborne.selenide.Selenide.$;
-import static io.qameta.allure.Allure.step;
-import static java.lang.String.format;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.openqa.selenium.By.id;
 
 public class IosSearchTests extends TestBase {
 
@@ -28,26 +23,4 @@ public class IosSearchTests extends TestBase {
                 .checkOutputText("Selenium");
     }
 
-    @DisplayName("Проверка функции поиска (ios)")
-    @Test
-    @Tag("ios")
-    void checkOutputTextTest() {
-        step("Click Text Button", () -> {
-            $(id("Text Button")).click();
-        });
-
-        step("Check initial state Output text", () -> {
-            assertEquals("Waiting for text input.", $(id("Text Output")).getText());
-        });
-
-        step(format("Set value %s in the input field and press enter", "Selenium"), () -> {
-            $(id("Text Input")).click();
-            $(id("Text Input")).sendKeys("Selenium");
-            $(id("Text Input")).pressEnter();
-        });
-
-        step("Check Output text", () -> {
-            assertEquals("Selenium", $(id("Text Output")).getText());
-        });
-    }
 }
